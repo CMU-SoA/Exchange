1fdad3278311348afb1ccc5e94db281d22eabeeb
Stephanie Guan <49424605+sguan12@users.noreply.github.com>
2021-08-08 13:28:04 -0700

    splash page

---
+++ script.js
@@ -0,0 +1,23 @@
+var launch = new Date("Aug 30, 2021 12:00:00").getTime();
+
+var x = setInterval(function() {
+  var today = new Date().getTime();
+  var distance = launch - today;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("countdownDays").innerHTML = days;
+  document.getElementById("countdownHours").innerHTML = hours;
+  document.getElementById("countdownMin").innerHTML = minutes;
+  document.getElementById("countdownSec").innerHTML = seconds;
+    
+  if (distance < 0) {
+    document.getElementById("countdownDays").innerHTML = 0;
+    document.getElementById("countdownHours").innerHTML = 0;
+    document.getElementById("countdownMin").innerHTML = 0;
+    document.getElementById("countdownSec").innerHTML = 0;
+  }
+}, 1000);
